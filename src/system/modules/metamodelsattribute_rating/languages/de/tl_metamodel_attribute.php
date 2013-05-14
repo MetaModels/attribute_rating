9f0e33ded70803d22f8f69f33cccd95b54fc01a8
Mini Model <minimodel@metamodel.me>
2013-05-14 10:15:48 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_rating/languages/de/tl_metamodel_attribute.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-14T00:11:39+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_emtpy']['0']     = 'Bild für "leeren Stern"';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_emtpy']['1']     = 'Bitte wählen Sie ein Bild aus, das als "leerer Stern" benutzt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_full']['0']      = 'Bild für "voller Stern"';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_full']['1']      = 'Bitte wählen Sie ein Bild aus, das als "gefüllter Stern" benutzt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_half']['0']      = 'Halbe Wertungen';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_half']['1']      = 'Falls angewählt sind auch 0.5-Schritte anstatt ganzer Zahlen möglich.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_hover']['0']     = 'Bild für Hovereffekt';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_hover']['1']     = 'Bitte wählen Sie ein Bild aus, das für einen Hovereffekt benutzt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_max']['0']       = 'Maximalwert';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_max']['1']       = 'Bitte geben Sie den Maximalwert für eine Bewertung ein.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['rating'] = 'Bewertung';
