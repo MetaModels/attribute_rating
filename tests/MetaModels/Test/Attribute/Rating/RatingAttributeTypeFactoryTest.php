aa310e0ae63b08b13807a6605a453419b2cd2e1a
Ingolf Steinhardt <info@e-spin.de>
2018-08-05 10:57:41 +0200

    hotfix author validation

--- tests/MetaModels/Test/Attribute/Rating/RatingAttributeTypeFactoryTest.php
+++ tests/MetaModels/Test/Attribute/Rating/RatingAttributeTypeFactoryTest.php
@@ -13,11 +13,10 @@
  * @package    MetaModels
  * @subpackage AttributeRating
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @author     David Greminger <david.greminger@1up.io>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2018 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_rating/blob/master/LICENSE LGPL-3.0
+ * @license    https://github.com/MetaModels/attribute_rating/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
