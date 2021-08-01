018655549db93bb568cf21743a56be413e19aaba
Ingolf Steinhardt <info@e-spin.de>
2021-08-01 18:51:30 +0200

    Switch to github-actions

--- tests/Test/Attribute/RatingTest.php
+++ tests/Test/Attribute/RatingTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_rating.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_rating/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -33,6 +33,8 @@ use MetaModels\IMetaModel;
 
 /**
  * Unit tests to test class Rating.
+ *
+ * @covers \MetaModels\AttributeRatingBundle\Attribute\Rating
  */
 class RatingTest extends TestCase
 {
