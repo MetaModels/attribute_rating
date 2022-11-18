e68b63fe0acd5bfc44d9817a04f4f18f8599b344
Mini Model <minimodel@metamodel.me>
2022-11-18 13:09:09 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/de/tl_metamodel_attribute.php
@@ -1,29 +1,35 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_rating.
+ *
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2022-11-18T13:08:54+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2013-05-14T00:11:39+02:00
+ * @copyright 2012-2022 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_rating/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
+
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_emtpy']['0']     = 'Bild für "leeren Stern"';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_emtpy']['1']     =
-    'Bitte wählen Sie ein Bild aus, das als "leerer Stern" benutzt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_emtpy']['1']     = 'Bitte wählen Sie ein Bild aus, das als "leerer Stern" benutzt werden soll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_full']['0']      = 'Bild für "voller Stern"';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_full']['1']      =
-    'Bitte wählen Sie ein Bild aus, das als "gefüllter Stern" benutzt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_full']['1']      = 'Bitte wählen Sie ein Bild aus, das als "gefüllter Stern" benutzt werden soll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_half']['0']      = 'Halbe Wertungen';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_half']['1']      =
-    'Falls angewählt sind auch 0.5-Schritte anstatt ganzer Zahlen möglich.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_half']['1']      = 'Falls angewählt, sind auch 0.5-Schritte anstatt ganzer Zahlen möglich.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_hover']['0']     = 'Bild für Hovereffekt';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_hover']['1']     =
-    'Bitte wählen Sie ein Bild aus, das für einen Hovereffekt benutzt werden soll.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_hover']['1']     = 'Bitte wählen Sie ein Bild aus, das für einen Hovereffekt benutzt werden soll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_max']['0']       = 'Maximalwert';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_max']['1']       =
-    'Bitte geben Sie den Maximalwert für eine Bewertung ein.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rating_max']['1']       = 'Bitte geben Sie den Maximalwert für eine Bewertung ein.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['rating'] = 'Bewertung';
+
